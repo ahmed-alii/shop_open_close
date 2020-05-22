3655966916b9a7cfdcccce6699cf4378f5ed85b7
ahmed <14140007@gift.edu.pk>
2020-05-22 22:20:19 +0500

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+
+let status;
+
+router.get('/', function (req, res) {
+    res.render('index');
+});
+
+router.post('/', function (req, res) {
+    status = req.body.shop;
+    console.log(req.body.shop)
+    res.json(status)
+})
+
+router.get('/api', function (req, res) {
+    res.json(status)
+})
+
+module.exports = router;
